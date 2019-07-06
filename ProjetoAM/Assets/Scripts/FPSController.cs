9cf175761e96f287313d8cfe6a9f4faa13790d23
MunchMunchi <43782986+MunchMunchi@users.noreply.github.com>
2019-07-06 17:14:50 +0100

    remade arena fixed movement

--- ProjetoAM/Assets/Scripts/FPSController.cs
+++ ProjetoAM/Assets/Scripts/FPSController.cs
@@ -10,16 +10,28 @@ public class FPSController : MonoBehaviour {
     public GameObject eyes;
     public GameObject shot;
 
+    public GameObject camera;
+
     public float fireRate, nextFire;
     private float moveFB, moveLR, rotX, rotY, vertVelocity;
     
     private bool hasJumped;
     public Transform shotSpawn;
-    
+
+       
+    // Audio
+
+    public AudioClip shoot1;
+    public AudioClip shoot2;
+    public AudioClip jump;
+
+    private AudioSource source;
+
 
     // Use this for initialization
     void Start () {
-
+        source = GetComponent<AudioSource>();
+        source.playOnAwake = false;
         player = GetComponent<CharacterController>();
 
 	}
@@ -33,6 +45,7 @@ public class FPSController : MonoBehaviour {
         if (Input.GetButtonDown("Jump"))
         {
             hasJumped = true;
+            source.PlayOneShot(jump);
         }
 
         if (Input.GetButton("Fire1") && Time.time > nextFire)
@@ -40,12 +53,19 @@ public class FPSController : MonoBehaviour {
             nextFire = Time.time + fireRate;
             var bullet = Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
 
+            if(Random.Range(0, 1) == 1)
+                source.PlayOneShot(shoot1);
+            else
+                source.PlayOneShot(shoot2);
+
+
+
+
             bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 15;
             Destroy(bullet, 2.0f);
 
             
         }
-
     }
 
     void Movement()
@@ -67,8 +87,10 @@ public class FPSController : MonoBehaviour {
         transform.Rotate(0, rotX, 0);
 
         //rodar a camera no eixo dos yy
-        eyes.transform.localRotation = Quaternion.Euler(0, 0, rotY);
-     
+        // eyes.transform.Rotate(0, rotX, rotY*0.01f);
+        camera.transform.localRotation = Quaternion.Euler(rotY, 0, 0);
+        //eyes.transform.localRotation = Quaternion.Euler(0, 0, rotY );
+
 
         //orientar o player na direção certa
         movement = transform.rotation * movement;
